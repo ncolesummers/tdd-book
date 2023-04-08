53764a3106b5e2ecece59b113a83c5a954eb5537
ncolesummers <nsummers72@gmail.com>
2023-04-08 12:10:09 -0700

    init

---
+++ go/money_test.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+    "testing"
+)
+
+func TestMultiplication(t *testing.T) {
+    fiver := Dollar{amount: 5}
+    tenner := fiver.Times(2)
+    if tenner.amount != 10 {
+        t.Errorf("Expected 10, got: [%d]", tenner.amount)
+    }
+}
+
+func TestMultiplicationInEuros(t *testing.T) {
+	tenEuros := Money{amount: 10, currency: "EUR"}
+	twentyEuros := tenEuros.Times(2)
+	if twentyEuros.amount != 20 {
+		t.Errorf("Expected 20, got: [%d]", twentyEuros.amount)
+	}
+	if twentyEuros.currency != "EUR" {
+		t.Errorf("Expected EUR, got: [%s]", twentyEuros.currency)
+	}
+}
+
+type Dollar struct {
+    amount int
+}
+
+type Money struct {
+	amount int
+	currency string
+}
+
+func (d Dollar) Times(multiplier int) Dollar {
+    return Dollar{amount: d.amount * multiplier}
+}
+
+
+func (m Money) Times(multiplier int) Money {
+	return Money{amount: m.amount * multiplier, currency: m.currency}
+}
