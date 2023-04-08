e31b3ab3fdb664b44f97f86c4a0a28af37c77fbc
ncolesummers <nsummers72@gmail.com>
2023-04-08 13:21:43 -0700

    feat: division and multiplication features done

--- go/money_test.go
+++ go/money_test.go
@@ -1,42 +1,45 @@
 package main
 
 import (
-    "testing"
+	"testing"
 )
 
-func TestMultiplication(t *testing.T) {
-    fiver := Dollar{amount: 5}
-    tenner := fiver.Times(2)
-    if tenner.amount != 10 {
-        t.Errorf("Expected 10, got: [%d]", tenner.amount)
-    }
+func TestMultiplicationInDollars(t *testing.T) {
+	fiver := Money{amount: 5, currency: "USD"}
+	actualResult := fiver.Times(2)
+	expectedResult := Money{amount: 10, currency: "USD"}
+	assertEqual(t, expectedResult, actualResult)
 }
 
 func TestMultiplicationInEuros(t *testing.T) {
 	tenEuros := Money{amount: 10, currency: "EUR"}
-	twentyEuros := tenEuros.Times(2)
-	if twentyEuros.amount != 20 {
-		t.Errorf("Expected 20, got: [%d]", twentyEuros.amount)
-	}
-	if twentyEuros.currency != "EUR" {
-		t.Errorf("Expected EUR, got: [%s]", twentyEuros.currency)
-	}
+	actualResult := tenEuros.Times(2)
+	expectedResult := Money{amount: 20, currency: "EUR"}
+	assertEqual(t, expectedResult, actualResult)
 }
 
-type Dollar struct {
-    amount int
+func TestDivision(t *testing.T) {
+	originalMoney := Money{amount: 4002, currency: "KRW"}
+	actualMoneyAfterDivision := originalMoney.Divide(4)
+	expectedMoneyAfterDivision := Money{amount: 1000.5, currency: "KRW"}
+	assertEqual(t, expectedMoneyAfterDivision, actualMoneyAfterDivision)
+}
+
+func assertEqual(t *testing.T, expected Money, actual Money) {
+	if expected != actual {
+		t.Errorf("Expected %+v Got %+v", expected, actual)
+	}
 }
 
 type Money struct {
-	amount int
+	amount   float64
 	currency string
 }
 
-func (d Dollar) Times(multiplier int) Dollar {
-    return Dollar{amount: d.amount * multiplier}
+func (m Money) Times(multiplier float64) Money {
+	return Money{amount: m.amount * multiplier, currency: m.currency}
 }
 
-
-func (m Money) Times(multiplier int) Money {
-	return Money{amount: m.amount * multiplier, currency: m.currency}
+func (m Money) Divide(divisor float64) Money {
+	return Money{amount: m.amount * (1 / divisor), currency: m.currency}
 }
